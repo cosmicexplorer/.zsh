5c1a986cd612ec67ecd9e810ba9618ad9e08b0e0
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-06-11 00:10:39 -0700

    massive overhaul to zsh config

--- find_closest_command_not_found.zsh
+++ find_closest_command_not_found.zsh
@@ -14,7 +14,7 @@ TRUNCATE_LENGTH=16              # must be multiple of two for pacsearch to
 stop_truncation="false"
 
 if [ "$2" = "-a" ]; then
-    stop_truncation="true"
+  stop_truncation="true"
 fi
 
 is_truncated="false"
@@ -25,17 +25,19 @@ searching for replacements...\033[1;0m"
 echo "$(bash -c "compgen -A function -abck" | grep "[[:alpha:]]" ; \
 	cat "$ZSH_DIR/aliases" | grep alias | \
 	grep -v "#" | grep -o " [[:alnum:]]*=" | grep -o "[[:alnum:]]*")" > \
-"$ZSH_DIR/commandNotFoundFile"
+     "$ZSH_DIR/commandNotFoundFile"
 
 if [ $stop_truncation = "false" ]; then
-    levenshtein_lines="$("$ZSH_DIR/use_levenshtein_for_command.py" $1 | wc -l)"
-    "$ZSH_DIR/use_levenshtein_for_command.py" $1 | head -n$TRUNCATE_LENGTH
-    if [ $levenshtein_lines -gt $TRUNCATE_LENGTH ]; then
-        is_truncated="true"
-        echo -e "\033[1;35mand more...\033[1;0m"
-    fi
+  levenshtein_lines="$("$ZSH_DIR/use_levenshtein_for_command.pl" $1 \
+"$ZSH_DIR/commandNotFoundFile" | wc -l)"
+  "$ZSH_DIR/use_levenshtein_for_command.pl" \
+    "$1" "$ZSH_DIR/commandNotFoundFile" | head -n$TRUNCATE_LENGTH
+  if [ $levenshtein_lines -gt $TRUNCATE_LENGTH ]; then
+    is_truncated="true"
+    echo -e "\033[1;35mand more...\033[1;0m"
+  fi
 else
-    "$ZSH_DIR/use_levenshtein_for_command.py" $1
+  "$ZSH_DIR/use_levenshtein_for_command.pl" "$1" "$ZSH_DIR/commandNotFoundFile"
 fi
 
 rm "$ZSH_DIR/commandNotFoundFile"
@@ -43,49 +45,49 @@ rm "$ZSH_DIR/commandNotFoundFile"
 # if internet available
 if [ "$(ip route ls)" != "" ] ; then
 
-    if hash yaourt 2>/dev/null; then
-        echo -e -n "\033[1;33msearching pacman and AUR...\033[1;0m"
-    else
-        echo -e -n "\033[1;33msearching pacman...\033[1;0m"
-    fi
+  if hash yaourt 2>/dev/null; then
+    echo -e -n "\033[1;33msearching pacman and AUR...\033[1;0m"
+  else
+    echo -e -n "\033[1;33msearching pacman...\033[1;0m"
+  fi
 
-    if [ $stop_truncation = "false" ]; then
-        if hash yaourt 2>/dev/null; then
-            pacsearch_numlines="$(yaourt -Ss $1 | wc -l)"
-        else
-            pacsearch_numlines="$(pacsearch $1 | wc -l)"
-        fi
-        if [ $pacsearch_numlines -eq 0 ]; then
-            echo -e "\033[1;31mnone found.\033[1;0m"
-        else
-            echo -n -e "\n"
-            if hash yaourt 2>/dev/null; then
-                yaourt -Ss --color $1 | head -n$TRUNCATE_LENGTH
-            else
-                pacsearch $1 | head -n$TRUNCATE_LENGTH
-            fi
-            if [ $pacsearch_numlines -gt $TRUNCATE_LENGTH ]; then
-                is_truncated="true"
-                echo -e "\033[1;33mand more...\033[1;0m"
-            fi
-        fi
+  if [ $stop_truncation = "false" ]; then
+    if hash yaourt 2>/dev/null; then
+      pacsearch_numlines="$(yaourt -Ss $1 | wc -l)"
     else
-        if hash yaourt 2>/dev/null; then
-            pacsearch_numlines="$(yaourt -Ss $1 --color | wc -l)"
-        else
-            pacsearch_numlines="$(pacsearch $1 | wc -l)"
-        fi
-        if [ $pacsearch_numlines -eq 0 ]; then
-            echo -e "\033[1;31mnone found.\033[1;0m"
-        else
-            echo
-            if hash yaourt 2>/dev/null; then
-                yaourt -Ss --color $1
-            else
-                pacsearch $1
-            fi
-        fi
+      pacsearch_numlines="$(pacsearch $1 | wc -l)"
     fi
+    if [ $pacsearch_numlines -eq 0 ]; then
+      echo -e "\033[1;31mnone found.\033[1;0m"
+    else
+      echo -n -e "\n"
+      if hash yaourt 2>/dev/null; then
+        yaourt -Ss --color $1 | head -n$TRUNCATE_LENGTH
+      else
+        pacsearch $1 | head -n$TRUNCATE_LENGTH
+      fi
+      if [ $pacsearch_numlines -gt $TRUNCATE_LENGTH ]; then
+        is_truncated="true"
+        echo -e "\033[1;33mand more...\033[1;0m"
+      fi
+    fi
+  else
+    if hash yaourt 2>/dev/null; then
+      pacsearch_numlines="$(yaourt -Ss $1 --color | wc -l)"
+    else
+      pacsearch_numlines="$(pacsearch $1 | wc -l)"
+    fi
+    if [ $pacsearch_numlines -eq 0 ]; then
+      echo -e "\033[1;31mnone found.\033[1;0m"
+    else
+      echo
+      if hash yaourt 2>/dev/null; then
+        yaourt -Ss --color $1
+      else
+        pacsearch $1
+      fi
+    fi
+  fi
 
 fi
 
---
+++ use_levenshtein_for_command.pl
@@ -0,0 +1,29 @@
+#!/bin/perl
+
+my $LEVENSHTEIN_CHECK_DIST = 4;
+
+use Text::Levenshtein qw(distance);
+
+my ($attemptedCommand, $filePath) = @ARGV;
+
+open(my $inhandle, "<", $filePath)
+  or die "$0: can't open $filePath for reading: $!";
+
+my @possibleCommands = ();
+
+while (<$inhandle>) {
+  if (distance($attemptedCommand, $_) < $LEVENSHTEIN_CHECK_DIST){
+    push @possibleCommands, $_;
+  }
+}
+
+close $inhandle;
+
+if (scalar @possibleCommands != 0) {
+  print "\n\033[1;35mdid you mean:\033[1;0m\n";
+  foreach (@possibleCommands) {
+    print "$_";
+  }
+} else {
+  die "\033[1;31mnone found.\033[1;0m";
+}
