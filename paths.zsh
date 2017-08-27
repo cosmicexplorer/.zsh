ff7172f405b2f0eb5ccf6305aa2936fedfd31b28
Danny McClanahan <danieldmcclanahan@gmail.com>
2017-08-27 02:53:35 -0700

    indeed

--- paths.zsh
+++ paths.zsh
@@ -2,6 +2,7 @@ add_path_if /usr/bin/{core_perl,vendor_perl}
 add_path_if "$HOME/.cabal/bin"
 add_path_before_if /usr/local/bin
 add_path_before_if "$HOME/.local/bin"
+add_path_before_if "$HOME/go/bin"
 
 export PASSWORD_STORE_DIR="$HOME"
 
